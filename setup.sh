e1102504c4b09bd7de14d3c1f795a9e1368183d0
Stefania La Vattiata Sanchez <stefanialavattiatasanchez@Stefanias-MacBook-Air.local>
2020-04-06 13:12:29 -0400

    Conda Environment

---
+++ setup.sh
@@ -0,0 +1,11 @@
+conda create --name pyforecast python=3.6
+source ~/anaconda3/etc/profile.d/conda.sh
+#source ~/miniconda/etc/profile.d/conda.sh
+conda activate pyforecast
+
+conda install -c anaconda numpy==1.16.1
+conda install -c anaconda scipy==1.2.1
+conda install -c anaconda scikit-learn==0.21.3
+conda install -c anaconda pandas==0.25.2
+conda install -c conda-forge matplotlib==3.1.1
+conda install -c anaconda seaborn==0.9.0
