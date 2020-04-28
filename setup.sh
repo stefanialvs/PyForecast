1d173dd69f46c6b5fc36428b1ad93dce4c7c6db2
Kin Gtz Olivares <kin.gtz.olivares@gmail.com>
2020-04-28 17:44:50 -0400

    Added ESRNN to benchmarks

--- setup.sh
+++ setup.sh
@@ -1,9 +1,10 @@
 conda create --name pyforecast python=3.6
-source ~/anaconda3/etc/profile.d/conda.sh
+source ~/opt/anaconda3/etc/profile.d/conda.sh
 #source ~/miniconda/etc/profile.d/conda.sh
 conda activate pyforecast
 
-conda install -c anaconda numpy==1.16.1
+pip install ESRNN
+#conda install -c anaconda numpy==1.16.1
 conda install -c anaconda scipy==1.2.1
 conda install -c anaconda scikit-learn==0.21.3
 conda install -c anaconda pandas==0.25.2
--- src/utils_ml.py
+++ src/utils_ml.py
@@ -10,6 +10,9 @@ from src.utils_data import prepare_data, seas_dict
 from src.utils_evaluation import *
 from src.utils_visualization import plot_grid_series, plot_distributions
 
+from ESRNN import ESRNN
+from ESRNN.utils_configs import get_config
+
 ######################################################################
 # PARSE USER INTERACTION
 ######################################################################
@@ -30,6 +33,36 @@ def models_metrics_filter(h, freq, models_filter, metrics_filter):
     metrics = {'RMSE': rmse, 'MAPE': mape, 'SMAPE': smape, 
                'MASE': mase, 'RMSSE': rmsse}
     
+    if 'ESRNN' in models_filter:
+        # Instantiate ESRNN
+        config = get_config(freq)
+        print(config)
+        models['ESRNN'] = ESRNN(max_epochs=7,
+                                batch_size=config['train_parameters']['batch_size'],
+                                freq_of_test=2,
+                                learning_rate=float(config['train_parameters']['learning_rate']),
+                                lr_scheduler_step_size=config['train_parameters']['lr_scheduler_step_size'],
+                                lr_decay=config['train_parameters']['lr_decay'],
+                                per_series_lr_multip=config['train_parameters']['per_series_lr_multip'],
+                                gradient_clipping_threshold=config['train_parameters']['gradient_clipping_threshold'],
+                                rnn_weight_decay=0.0,
+                                noise_std=0.000001,
+                                level_variability_penalty=config['train_parameters']['level_variability_penalty'],
+                                testing_percentile=config['train_parameters']['testing_percentile'],
+                                training_percentile=config['train_parameters']['training_percentile'],
+                                ensemble=False,
+                                max_periods=config['data_parameters']['max_periods'],
+                                seasonality=config['data_parameters']['seasonality'],
+                                input_size=config['data_parameters']['input_size'],
+                                output_size=config['data_parameters']['output_size'],
+                                frequency=config['data_parameters']['frequency'],
+                                cell_type=config['model_parameters']['cell_type'],
+                                state_hsize=config['model_parameters']['state_hsize'],
+                                dilations=config['model_parameters']['dilations'],
+                                add_nl_layer=config['model_parameters']['add_nl_layer'],
+                                random_seed=config['model_parameters']['random_seed'],
+                                device='cpu')
+        
     # Filtered models and metrics
     models = {model_name: models[model_name] for model_name in models_filter}
     metrics = {metric_name: metrics[metric_name] for metric_name in metrics_filter}
@@ -70,19 +103,27 @@ def ml_pipeline(directory, h, freq, models_filter, metrics_filter, progress_bar)
     # Fit and predict benchmark models
     preds = [y_test_df.y]
     print("\n Fitting models")
-    k = 0
     progress_bar['maximum']=len(models.items()) + len(metrics.items())
+    progress_bar['value']=0
     for model_name, model in models.items():
         print(model_name)
-        panel_model = PanelModel(model)
-        panel_model.fit(X_train_df, y_train_df)
-        mod_preds = panel_model.predict(X_test_df)
+        
+        if not model_name=='ESRNN':
+            panel_model = PanelModel(model)
+            panel_model.fit(X_train_df, y_train_df)
+            mod_preds = panel_model.predict(X_test_df)
+        else:
+            Xtrain_df = X_train_df.reset_index()
+            ytrain_df = y_train_df.reset_index()
+            Xtest_df = X_test_df.reset_index()
+            
+            model.fit(Xtrain_df, ytrain_df)
+            mod_preds = model.predict(Xtest_df)
         
         mod_preds.name = model_name
         preds.append(mod_preds)
-        k += 1
-        progress_bar['value']=k
-        progress_bar.update()
+        #progress_bar['value']+=1
+        #progress_bar.update()
     
     # Merge y_df for visualization purpose
     y_hat_df = pd.concat(preds, axis=1)
